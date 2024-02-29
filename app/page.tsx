e0cb127a8927b4ee11d9b7b724ecb6a35782d7a0
shebll <shebblloll@gmail.com>
2024-02-29 16:31:38 +0200

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import LoginForm from "./_components/LoginForm";
 
 export default function Home() {
   return (
-    <main className="container mx-auto flex flex-col gap-10  justify-center items-center py-[85px]">
+    <main className="container mx-auto flex flex-col gap-10  justify-center items-center py-[15px] lg:py-[25px]">
       <div className="hero-card">
         <div className="flex flex-col items-center gap-6">
+          <Image src={"/logo.png"} width={180} height={100} alt="logo png" />
           <h1 className="text-[32px] font-bold text-white text-center bg-blue-600 rounded w-fit px-12 py-1">
             THE BEST
           </h1>
@@ -14,8 +16,8 @@ export default function Home() {
           <LoginForm />
         </div>
       </div>
-      <div className=" flex flex-col gap-4 ">
-        <p className="text-[16px] lg:text-[24px] font-semibold text-[#dfdfdf] max-w-[300px] lg:max-w-[700px] text-center backdrop-blur-sm bg-black/70 px-6 py-2">
+      <div className=" flex flex-col gap-4 p-2">
+        <p className="text-[18px] lg:text-[24px] font-semibold text-[#dfdfdf]  lg:max-w-[700px] text-center backdrop-blur-sm bg-[#c09238] px-6 py-2 ">
           من أفضل موقع لتنظيم فرق وإنشاء دورياتك الرياضية بكل سهولة ويسر. قم
           بإدارة الفرق، وجدولة المباريات، وتتبع النتائج ، وتفاعل مع اللاعبين، كل
           ذلك في مكان واحد. اكتشف الآن كيف يمكن لموقعنا أن يسهل عليك تنظيم أحلى
