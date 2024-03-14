e2cab02353194ffa886d35b2c2cf546b920659ad
shebll <shebblloll@gmail.com>
2024-03-14 15:48:34 +0200

    fix form data issues

--- action/createTeam.ts
+++ action/createTeam.ts
@@ -2,20 +2,10 @@
 
 export const createTeam = async (formData: FormData) => {
   console.log(formData);
-  const filteredFormData = new FormData();
-  for (const [key, value] of formData.entries()) {
-    if (value instanceof File) {
-      if (value.size > 0) {
-        filteredFormData.append(key, value);
-      }
-    } else {
-      filteredFormData.append(key, value);
-    }
-  }
-  console.log(filteredFormData);
+
   const response = await fetch(
     "https://the-best-football.onrender.com/team/create",
-    { method: "POST", body: filteredFormData }
+    { method: "POST", body: formData }
   );
   const responseData = await response.json();
   console.log(responseData);
